7b39948085d26cbe37a004205f089470b37a4244
Crowdbotics <team@crowdbotics.com>
2024-04-17 17:59:14 +0000

    Initial commit

---
+++ backend/jason_title_insuran_47576/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for jason_title_insuran_47576 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "jason_title_insuran_47576.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jason-title-insuran-47576",
+  url: "https://jason-title-insuran-47576.botics.co",
+  api: "https://jason-title-insuran-47576.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
